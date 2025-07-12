2975d6c639563fcac24f8a7ac275fa240238368d
Galandel <17754014+Galandel@users.noreply.github.com>
2025-07-12 17:57:02 -0600

    Adding LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,8 +1,8 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
-    def testrepr(self):
+    def test_repr(self):
         tag = 'p'
         value = 'Hello my name is:'
         children = None
@@ -11,7 +11,7 @@ class TestHTMLNode(unittest.TestCase):
         expected_output = f"HTLMNode({tag}, {value}, {children}, {props})"
         self.assertEqual(expected_output, repr(node))
     
-    def testprops_to_html(self):
+    def test_props_to_html(self):
         tag = 'p'
         value = 'Hello my name is:'
         children = None
@@ -28,6 +28,30 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode(tag, value, children, props)
         with self.assertRaises(NotImplementedError):
             node.to_html()
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "google", {"href": "www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="www.google.com">google</a>')
+
+    def test_value_exception(self):
+        node = LeafNode("a",None, {"href": "www.google.com"})
+        with self.assertRaises(ValueError):
+            node.to_html()
+    
+    def test_leaf_repr(self):
+        node = LeafNode("a", "google", {"href": "www.google.com"})
+        self.assertEqual(
+            repr(node),
+            "LeafNode(a, google, {'href': 'www.google.com'})"
+        )
+    
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
 if __name__ == "__main__":
     unittest.main()
