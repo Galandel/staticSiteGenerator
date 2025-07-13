b0bae3d92076d697c9aab855bb9ce22e804c4a42
Galandel <17754014+Galandel@users.noreply.github.com>
2025-07-13 15:29:04 -0600

    extract markdown images and links functions

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,5 @@
 from textnode import TextType, TextNode
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -20,3 +21,16 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(segment, text_type))
     # print(f"DEBUG: new_nodes: {new_nodes}")
     return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)",text)
+
+# text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+# print(extract_markdown_images(text))
+
+
+# text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+# print(f"DEBUG: {extract_markdown_links(text)}")
--- src/test_functiopns.py
+++ src/test_functiopns.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from functions import split_nodes_delimiter
+from functions import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 class TestTextNode(unittest.TestCase):
     def test_split_nodes_delimiter_code(self):
@@ -56,6 +56,35 @@ class TestTextNode(unittest.TestCase):
             TextNode(" more", TextType.TEXT),
         ]
         self.assertEqual(new_nodes, expected_output)
+    
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)")
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+    
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links("This is text with a [link](https://www.youtube.com/@bootdotdev)")
+        self.assertListEqual([("link", "https://www.youtube.com/@bootdotdev")], matches)
+
+    def test_extract_multiple_images(self):
+        text = "Here's ![first](url1.png) and ![second](url2.jpg)"
+        matches = extract_markdown_images(text)
+        self.assertListEqual([("first", "url1.png"), ("second", "url2.jpg")], matches)
+
+    def test_extract_mixed_content(self):
+        text = "A ![image](img.png) and a [link](site.com)"
+        # Should only get the link, not the image
+        matches = extract_markdown_links(text)
+        self.assertListEqual([("link", "site.com")], matches)
+    
+    def test_extract_mixed_content_images(self):
+        text = "Here's ![first](url1.png) and ![second](url2.jpg) from [link](https://google.com)"
+        matches = extract_markdown_images(text)
+        self.assertListEqual([("first", "url1.png"), ("second", "url2.jpg")], matches)
+    
+    def test_extract_empty_alt_images(self):
+        text = "here's ![](url1.png) and ![second](url2.jpeg)"
+        matches = extract_markdown_images(text)
+        self.assertListEqual([("", "url1.png"), ("second", "url2.jpeg")], matches)
 
 
 if __name__ == "__main__":
